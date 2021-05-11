24c8225878769ab0e4a2b6d93837ff5e6b0cef89
Paddy Cassidy <pjca@deakin.edu.au>
2021-05-11 18:18:38 +1000

    Added function to scrape the top 25 titles of  r/asxbets every 30 seconds

--- crawler.js
+++ crawler.js
@@ -2,6 +2,10 @@
 const express = require('express');
 //moment is a library for working with dates (may not be required)
 const moment = require('moment');
+//cors library
+const cors = require('cors');
+//library for crawler
+const puppeteer = require('puppeteer');
 
 const app = express();
 
@@ -14,7 +18,34 @@ const log = function(message){
     console.log('[Server] '+ time + ' ' + message)
 }
 
+//config cors 
+app.use(cors())
 
+
+function scrape(){
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const url = 'https://old.reddit.com/r/asxbets/';
+    await page.goto(url);
+    
+    //gets the top 25 post titles on r/asxbets and stores in array called postTitles
+    const postTitles = await page.evaluate(() => 
+    Array.from(document.querySelectorAll('p[class="title"]')).map(post => post.textContent.trim())
+    )
+
+    console.log(postTitles);
+
+    await browser.close();
+})();
+
+}
+//refresh the data every 30 seconds
+setInterval(scrape,30000);
+
+//endpoint for getting the crawled data set
+
+//server
 const port = 8080;
 app.listen(port)
-log('Server is listening on port: ' + port)
+log('CORS-enabled web server is listening on port: ' + port)
