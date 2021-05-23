7990ad1e1447f5d8700faa14c6e3eef0f28fdec1
Paddy Cassidy <pjca@deakin.edu.au>
2021-05-23 18:55:14 +1000

    Debug cors issue on /crawler endpoint

--- crawler.js
+++ crawler.js
@@ -22,9 +22,6 @@ const log = function(message){
 
 //config cors 
 app.use(cors())
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
 
 //Use puppeteer to scrape reddit
 const scrape =  async () =>  {
